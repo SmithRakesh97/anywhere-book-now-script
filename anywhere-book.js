42755b228695c7155a0310eb9df34d71708d9dd6
SMITH RAKESH <smith.muni@anywhere.co>
2026-02-05 14:07:18 +0530

    Update CSS link to new asset URL

--- anywhere-book.js
+++ anywhere-book.js
@@ -146,7 +146,8 @@
 		}
 		const link = document.createElement('link');
 		link.rel = 'stylesheet';
-		link.href = 'https://smithrakesh97.github.io/anywhere-book-now-css/anywhere-book-now.css';
+		link.href =
+			'https://assets.serviceforge.com/integration/book-now/staging/v1/anywhere-book-now.css';
 		document.head.appendChild(link);
 
 		const triggers = document.querySelectorAll('#Anywhere_button_iframe');
